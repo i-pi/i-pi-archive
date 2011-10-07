cfc881f2b5738204c963936648d2002b6ce285df
joshmore <joshua.more@chem.ox.ac.uk>
2011-10-07 11:27:15 +0100

    Now I've done the same change to the langevin thermostat. This basically means that the private variable is held in the base class, rather than defining a new private variable in the derived class. I don't know which version I actually prefer...

--- src/engine/langevin.py
+++ src/engine/langevin.py
@@ -6,25 +6,23 @@ class Thermo_Langevin(thermostat.Thermostat):
    
    def compute_TS(self):
       print "Re-computing propagator"
-      self.__T=math.exp(-self.__dt/self.__tau)
-      self.__S=math.sqrt(self.__temp*(1-self.__T**2))
+      self.__T=math.exp(-self.dt_Base/self.__tau)
+      self.__S=math.sqrt(self.temp*(1-self.__T**2))
     
    @property
    def dt(self):
       print "langevin getter called"
-      return self.__dt
+      return self.dt_Base
      
    @dt.setter
    def dt(self,new):
       print "Thermo_Langevin setter called"
-      self.__dt = new
+      self.dt_Base = new
       self.compute_TS()
    
    def __init__(self):
       thermostat.Thermostat.__init__(self)
-      self.__temp=self.temp
       self.__tau=1.0
       self.__T=1.0
       self.__S=1.0
-      self.__dt = self.dt_Base
 
