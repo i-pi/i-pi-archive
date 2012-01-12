5e426c03df8d280154bff952ca1e8b85936fcb31
joshmore <joshua.more@chem.ox.ac.uk>
2012-01-12 11:11:35 +0000

    Finished documenting depend.py

--- src/utils/depend.py
+++ src/utils/depend.py
@@ -23,9 +23,10 @@ Classes:
    depend_array: Depend class for arrays.
    synchronizer: Class that holds the different objects that are related to each
       other and keeps track of which property has been set manually.
-   dobject: An extension of the standard library object that overloads __get__
-      and __set__, so that we can use the standard syntax for setting and 
-      getting the depend object, i.e. foo = value, not foo.set(value).
+   dobject: An extension of the standard library object that overloads 
+      __getattribute__ and __setattribute__, so that we can use the 
+      standard syntax for setting and getting the depend object, 
+      i.e. foo = value, not foo.set(value).
 
 Functions:
    dget: Gets the dependencies of a depend object.
@@ -95,7 +96,7 @@ class depend_base(object):
    """ 
 
    def __init__(self, name, synchro=None, func=None, dependants=None, dependencies=None, tainted=None):
-      """Initialises depend base.
+      """Initialises depend_base.
 
       An unusual initialisation routine, as it has to be able to deal with the
       depend array mechanism for returning slices as new depend arrays.
@@ -121,7 +122,7 @@ class depend_base(object):
          dependants: An optional list containing objects that depend on self.
          dependencies: An optional list containing objects that self 
             depends upon.
-   """
+      """
 
       self._dependants=[]
       if tainted is None:
@@ -159,6 +160,7 @@ class depend_base(object):
             irm=idep
       if irm >=0: 
          self._dependants.pop(irm)
+      #self._dependants = [dep for dep in self._dependants if dep is not rmdep]
          
    def add_dependant(self, newdep, tainted=True):
       """Adds a dependant property.
@@ -268,11 +270,40 @@ class depend_base(object):
 
 
 class depend_value(depend_base):
-   def __init__(self, value=None, name="", synchro=None, func=None, dependants=None, dependencies=[], tainted=None):
+   """Scalar class for dependency handling.
+
+   Attributes:
+      _value: The value associated with self.
+   """ 
+
+   def __init__(self, name, value=None, synchro=None, func=None, dependants=None, dependencies=None, tainted=None):
+      """Initialises depend_value.
+
+      Args:
+         name: A string giving the name of self.
+         value: The value of the object. Optional.
+         tainted: An optional array giving the tainted flag. Default is [True].
+         func: An optional argument that can be specified either by a function 
+            name, or for synchronized values a dictionary of the form 
+            {"name": function name}; where "name" is one of the other
+            synched objects and function name is the name of a function to
+            get the object "name" from self.
+         synchro: An optional synchronizer object.
+         dependants: An optional list containing objects that depend on self.
+         dependencies: An optional list containing objects that self 
+            depends upon.
+      """
+
       self._value=value
       super(depend_value,self).__init__(name, synchro, func, dependants, dependencies, tainted)
 
    def get(self):
+      """Returns value, after recalculating if necessary.
+
+      Overwrites the standard method of getting value, so that value
+      is recalculated if tainted.
+      """
+
       if self.tainted():  
          self.update_auto()
          self.taint(taintme=False)
@@ -280,41 +311,115 @@ class depend_value(depend_base):
       return self._value
       
    def __get__(self, instance, owner):
+      """Overwrites standard get function."""
+
       return self.get() 
 
    def set(self, value, manual=True):
+      """Alters value and taints dependencies.
+
+      Overwrites the standard method of setting value, so that dependent 
+      quantities are tainted, and so we check that computed quantities are not
+      manually updated.
+      """
+
       self._value=value
       self.taint(taintme=False)
       if (manual):
          self.update_man()
       
    def __set__(self, instance, value): 
+      """Overwrites standard set function."""
+
       self.set(value)   
 
 
 class depend_array(np.ndarray, depend_base):
-   def __new__(cls, value, name="", synchro=None, func=None, dependants=None, dependencies=None, tainted=None, storage=None):
+   """Array class for dependency handling.
+
+   Differs from depend_value as arrays handle getting items in a different
+   way to scalar quantities, and as there needs to be support for slicing an
+   array. Initialisation is also done in a different way for ndarrays.
+
+   Attributes:
+      _storage: The base array. Equal to value unless self is a slice.
+   """
+
+   def __new__(cls, value, name, synchro=None, func=None, dependants=None, dependencies=None, tainted=None, storage=None):
+      """Creates a new array from a template.
+
+      Called whenever a new instance of depend_array is created. Casts the
+      array base into an appropriate form before passing it to 
+      __array_finalize__().
+
+      Args:
+         See __init__().
+      """
+
       obj = np.asarray(value).view(cls)
       return obj
 
-   def __init__(self, value, name="", synchro=None, func=None, dependants=None, dependencies=None, tainted=None, storage=None):
+   def __init__(self, value, name, synchro=None, func=None, dependants=None, dependencies=None, tainted=None, storage=None):
+      """Initialises depend_array.
+
+      Note that this is only called when a new array is created by an 
+      explicit constructor.
+
+      Args:
+         name: A string giving the name of self.
+         value: The array to serve as the memory base.
+         tainted: An optional array giving the tainted flag. Default is [True].
+         func: An optional argument that can be specified either by a function 
+            name, or for synchronized values a dictionary of the form 
+            {"name": function name}; where "name" is one of the other
+            synched objects and function name is the name of a function to
+            get the object "name" from self.
+         synchro: An optional synchronizer object.
+         dependants: An optional list containing objects that depend on self.
+         dependencies: An optional list containing objects that self 
+            depends upon.
+         storage: An optional array giving the base array if the array is a
+            slice.
+      """
+
       super(depend_array,self).__init__(name, synchro, func, dependants, dependencies, tainted)
       if storage is None:
          self._storage=value
       else:
-         self._storage=storage  #keeps track of where the original data is pointing, as automatic updates should access there
+         self._storage=storage
             
    def __array_finalize__(self, obj):  
-      depend_base.__init__(self)  #explicitly initialize in case we got here
+      """Deals with properly creating some arrays.
+
+      In the case where a function acting on a depend array returns a ndarray, 
+      this casts it into the correct form and gives it the 
+      depend machinery for other methods to be able to act upon it. New
+      depend_arrays will next be passed to __init__ ()to be properly 
+      initialized, but some ways of creating arrays do not call __new__() or
+      __init__(), so need to be initialized.
+      """
+
+      depend_base.__init__(self, name="") 
       self._storage=self
    
    # whenever possible in compound operations just return a regular ndarray
    __array_priority__=-1.0  
       
    def reshape(self, newshape):
+      """Changes the shape of the base array.
+
+      Args:
+         newshape: A tuple giving the desired shape of the new array.
+
+      Returns:
+         A depend_array with the dimensions given by newshape.
+      """
+
       return depend_array(self.base.reshape(newshape), name=self._name, synchro=self._synchro, func=self._func, dependants=self._dependants, tainted=self._tainted, storage=self._storage)  
 
    def flatten(self):
+      """Makes the base array one dimensional."""
+
       return self.reshape(self.size)
    
    @staticmethod
@@ -327,8 +432,9 @@ class depend_array(np.ndarray, depend_base):
       the global array.
 
       Arguments:
-         index : the index to be checked
-         depth : the rank of the array which is being accessed      
+         index: the index to be checked.
+         depth: the rank of the array which is being accessed. Default value
+            is 1. 
       
       Returns:
          A logical stating whether a __get__ instruction based
@@ -344,6 +450,16 @@ class depend_array(np.ndarray, depend_base):
       return False
       
    def __getitem__(self,index):
+      """Returns value[index], after recalculating if necessary.
+
+      Overwrites the standard method of getting value, so that value
+      is recalculated if tainted. Scalar slices are returned as an ndarray, 
+      so without depend machinery.
+
+      Args:
+         index: A slice variable giving the appropriate slice to be read.
+      """
+
       if self.tainted():  
          self.update_auto()
          self.taint(taintme=False)
@@ -354,16 +470,35 @@ class depend_array(np.ndarray, depend_base):
          return depend_array(self.base[index], name=self._name, synchro=self._synchro, func=self._func, dependants=self._dependants, tainted=self._tainted, storage=self._storage)
 
    def __getslice__(self,i,j):
+      """Overwrites standard get function."""
+
       return self.__getitem__(slice(i,j,None))
 
    def get(self):
+      """Alternative to standard get function."""
+
       return self.__getitem__(slice(None,None,None))
             
    def __get__(self, instance, owner):
+      """Overwrites standard get function."""
+
       return self.__getitem__(slice(None,None,None))
 
    def __setitem__(self,index,value,manual=True):      
-      self.taint(taintme=False)      
+      """Alters value[index] and taints dependencies.
+
+      Overwrites the standard method of setting value, so that dependent 
+      quantities are tainted, and so we check that computed quantities are not
+      manually updated.
+
+      Args:
+         index: A slice variable giving the appropriate slice to be read.
+         value: The new value of the slice.
+         manual: Optional boolean giving whether the value has been changed
+            manually. True by default.
+      """
+
+      self.taint(taintme=False)
       if manual:
          self.base[index]=value
          self.update_man()
@@ -371,32 +506,107 @@ class depend_array(np.ndarray, depend_base):
          self._storage[index]=value
       
    def __setslice__(self,i,j,value):
+      """Overwrites standard set function."""
+
       return self.__setitem__(slice(i,j),value)
 
    def set(self, value, manual=True):
+      """Alterative to standard set function.
+
+      Args:
+         See __setitem__().
+      """
+
       self.__setitem__(slice(None,None),value=value,manual=manual)    
 
    def __set__(self, instance, value): 
+      """Overwrites standard set function."""
+
       self.__setitem__(slice(None,None),value=value)
 
 def dget(obj,member):
+   """Takes an object and retrieves one of its attributes.
+
+   Note that this is necessary as calling it in the standard way calls the
+   __get__() function of member.
+
+   Args:
+      obj: A user defined class.
+      member: A string giving the name of an attribute of obj.
+
+   Exceptions:
+      KeyError: If member is not an attribute of obj.
+
+   Returns:
+      obj.member.
+   """
+
    return obj.__dict__[member]
    
 def dset(obj,member,value,name=None):
+   """Takes an object and sets one of its attributes.
+
+   Necessary for editing any depend object, and should be used for
+   initialising them as well, as often initialization occurs more than once, 
+   with the second time effectively being an edit.
+
+   Args:
+      obj: A user defined class.
+      member: A string giving the name of an attribute of obj.
+      value: The new value of member.
+      name: New name of member.
+
+   Exceptions:
+      KeyError: If member is not an attribute of obj.
+   """
+
    obj.__dict__[member]=value
    if not name is None: 
       obj.__dict__[member]._name=name
    
 def depstrip(deparray):
+   """Removes dependencies from a depend_array.
+
+   Takes a depend_array and returns its value as a ndarray, effectively
+   stripping the dependencies from the ndarray. This speeds up a lot of
+   calculations involving these arrays. Must only be used if the value of the
+   array is not going to be changed.
+
+   Args:
+      deparray: A depend_array.
+   
+   Returns:
+      A ndarray with the same value as deparray.
+   """
+
    return deparray.view(np.ndarray)
 
 def deppipe(objfrom,memberfrom,objto,memberto):
-   dfrom=dget(objfrom,memberfrom)
-   dto=dget(objto,memberto) 
-   dto._func=lambda : dfrom.get()
+   """Synchronizes two depend objects.
+
+   Takes two depend objects, and makes one of them depend on the other in such
+   a way that both keep the same value. Used for attributes such as temperature
+   that are used in many different modules, and so need different depend objects
+   in each, but which should all have the same value.
+
+   Args:
+      objfrom: An object containing memberfrom.
+      memberfrom: The base depend object.
+      objto: An object containing memberto.
+      memberto: The depend object that should be equal to memberfrom.
+   """
+
+   dfrom = dget(objfrom,memberfrom)
+   dto = dget(objto,memberto) 
+   dto._func = lambda : dfrom.get()
    dto.add_dependency(dfrom)
    
 def depcopy(objfrom,memberfrom,objto,memberto):
+   """Copies the dependencies of one depend object to another.
+
+   Args:
+      See deppipe.
+   """
    dfrom=dget(objfrom,memberfrom)
    dto=dget(objto,memberto)
    dto._dependants=dfrom._dependants
@@ -408,13 +618,29 @@ def depcopy(objfrom,memberfrom,objto,memberto):
    
 
 class dobject(object): 
+   """Class that allows standard notation to be used for depend objects."""
+ 
    def __getattribute__(self, name):
+      """Overwrites standard __getattribute__().
+
+      This changes the standard __getattribute__() function of any class that
+      subclasses dobject such that depend objects are called with their own
+      __get__() function rather than the standard one.
+      """
+
       value = object.__getattribute__(self, name)
       if hasattr(value, '__get__'):
          value = value.__get__(self, self.__class__)
       return value
 
    def __setattr__(self, name, value):
+      """Overwrites standard __setattribute__().
+
+      This changes the standard __setattribute__() function of any class that
+      subclasses dobject such that depend objects are called with their own
+      __set__() function rather than the standard one.
+      """
+
       try:
          obj = object.__getattribute__(self, name)
       except AttributeError:
