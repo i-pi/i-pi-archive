a3c24a72c85b4cbe3a198a68a38454966bc532b5
Michele Ceriotti <michele.ceriotti@gmail.com>
2013-06-18 17:37:47 +0100

    A couple of pointless edits

--- src/engine/properties.py
+++ src/engine/properties.py
@@ -211,7 +211,7 @@ class Properties(dobject):
       "cell_h": {    "dimension" : "length",
                       "help": "Gives cell vector matrix. Returns the 6 components in the form [xx, yy, zz, xy, xz, yz].",
                       "size": 6,
-                      "func": (lambda: self.flatten(self.cell.h))},
+                      "func": (lambda: self.tensor2vec(self.cell.h))},
       "cell_abcABC": {"dimension" : "undefined",
                       "help": "Gives the lengths of the cell vectors and the angles between them in degrees as a list of the form [a, b, c, A, B, C], where A is the angle between the sides of length b and c in degrees, and B and C are defined similarly. Since there are a mixture of different units, these can only be output in atomic-units.",
                       "size": 6,
@@ -258,33 +258,33 @@ class Properties(dobject):
       "stress_md": {  "dimension": "pressure",
                       "size" : 6,
                       "help": "The simulation stress tensor. Returns the 6 components in the form [xx, yy, zz, xy, xz, yz].",
-                      "func": (lambda: self.flatten((self.forces.vir + self.nm.kstress)/self.cell.V))},
+                      "func": (lambda: self.tensor2vec((self.forces.vir + self.nm.kstress)/self.cell.V))},
       "pressure_md": {"dimension": "pressure",
                       "help": "The simulation pressure.",
                       "func": (lambda: np.trace((self.forces.vir + self.nm.kstress)/(3.0*self.cell.V)))},
       "kstress_md":  {"dimension": "pressure",
                       "size" : 6,
                       "help": "The simulation kinetic stress tensor. Returns the 6 components in the form [xx, yy, zz, xy, xz, yz].",
-                      "func": (lambda: self.flatten(self.nm.kstress/self.cell.V))},
+                      "func": (lambda: self.tensor2vec(self.nm.kstress/self.cell.V))},
       "virial_md": {  "dimension": "pressure",
                       "size" : 6,
                       "help": "The simulation virial tensor. Returns the 6 components in the form [xx, yy, zz, xy, xz, yz].",
-                      "func": (lambda: self.flatten(self.forces.vir/self.cell.V))},
+                      "func": (lambda: self.tensor2vec(self.forces.vir/self.cell.V))},
       "stress_cv": {  "dimension": "pressure",
                       "size" : 6,
                       "help": "The physical system stress tensor.  Returns the 6 components in the form [xx, yy, zz, xy, xz, yz].",
-                      "func": (lambda: self.flatten(self.forces.vir + self.kstress_cv())/(self.cell.V*self.beads.nbeads))},
+                      "func": (lambda: self.tensor2vec(self.forces.vir + self.kstress_cv())/(self.cell.V*self.beads.nbeads))},
       "pressure_cv": {"dimension": "pressure",
                       "help": "The physical pressure of the system.",
                       "func": (lambda: np.trace(self.forces.vir + self.kstress_cv())/(3.0*self.cell.V*self.beads.nbeads))},
       "kstress_cv":  {"dimension": "pressure",
                       "size" : 6,
                       "help": "The physical system kinetic stress tensor. Returns the 6 components in the form [xx, yy, zz, xy, xz, yz].",
-                      "func": (lambda: self.flatten(self.kstress_cv()/(self.cell.V*self.beads.nbeads)))},
+                      "func": (lambda: self.tensor2vec(self.kstress_cv()/(self.cell.V*self.beads.nbeads)))},
       "virial_cv": {  "dimension": "pressure",
                       "size" : 6,
                       "help": "The physical system virial stress tensor. Returns the 6 components in the form [xx, yy, zz, xy, xz, yz].",
-                      "func": (lambda: self.flatten(self.forces.vir/(self.cell.V*self.beads.nbeads)))},
+                      "func": (lambda: self.tensor2vec(self.forces.vir/(self.cell.V*self.beads.nbeads)))},
       "yamamoto": {   "help": "Gives the estimators required to calculate the Yamamoto finite difference approximation to the kinetic energy and constant volume heat capacity. Returns eps_v and eps_v', as defined in Takeshi M. Yamamoto, Journal of Chemical Physics, 104101, 123 (2005). As the two estimators have a different dimension, this can only be output in atomic units. Takes one argument, 'fd_delta', which gives the value of the finite difference parameter used. It defaults to " + str(-self._DEFAULT_FINDIFF) + ". If the value of 'fd_delta' is negative, then its magnitude will be reduced automatically by the code if the finite difference error becomes too large.",
                       'func': self.get_yama_estimators,
                       "size": 2},
@@ -354,6 +354,13 @@ class Properties(dobject):
       else:
          return pkey["func"](*arglist,**kwarglist)
 
+   def tensor2vec(self, tensor):
+      """Takes a 3*3 symmetric tensor and returns it as a 1D array,
+      containing the elements [xx, yy, zz, xy, xz, yz].
+      """
+
+      return np.array([tensor[0,0], tensor[1,1], tensor[2,2], tensor[0,1], tensor[0,2], tensor[1,2]])
+
    def get_atom_vec(self, prop_vec, atom="", bead="-1"):
       """Gives a vector for one atom.
 
@@ -376,13 +383,6 @@ class Properties(dobject):
          return atom_vec/float(self.beads.nbeads)
       else:
          return prop_vec[bead,3*atom:3*(atom+1)]
-
-   def flatten(self, vec_2D):
-      """Takes a 3*3 upper-triangular array and returns it as a 1D array,
-      of the form [xx, yy, zz, xy, xz, yz].
-      """
-
-      return np.array([vec_2D[0,0], vec_2D[1,1], vec_2D[2,2], vec_2D[0,1], vec_2D[0,2], vec_2D[1,2]])
 
    def get_rg(self, atom=""):
       """Calculates the radius of gyration of the ring polymers.
