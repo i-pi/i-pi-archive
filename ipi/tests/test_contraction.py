63c0349d718a660bcf97d56160b5685c9df63f7b
Thomas Spura <thomas.spura@gmail.com>
2013-08-05 11:06:05 +0200

    Also test position rescaling properly

--- ipi/tests/test_contraction.py
+++ ipi/tests/test_contraction.py
@@ -18,11 +18,34 @@ def get_identicals(q):
     return ret
 
 
-def check_up_and_down_scaling(n, q):
+def check_up_and_down_scaling_pos(n, q):
     """Check if q can be upscaled to n beads and downscaled again.
     """
+    rescale = nmtransform.mk_rs_matrix(q.shape[0], n)
+    msk = get_identicals(q)
     print "Initial position of the bead system:"
-    print q, q.shape
+    print q, q.shape, msk, rescale.shape
+
+    # rescale up to the n beads
+    beads_n = np.dot(rescale, q)#*n/q.shape[0]
+    print "Upscaled to %d beads:"%n
+    print beads_n, beads_n.shape
+
+    rescale = nmtransform.mk_rs_matrix(n, q.shape[0])
+    beads_final = np.dot(rescale, beads_n)
+    print "Final position of the bead system:"
+    print beads_final
+
+    for bead in beads_n:
+        for i, m in enumerate(msk):
+            if m:
+                assert_equals(bead[i], q[0][i])
+    assert_equals(q, beads_final)
+
+
+def check_up_and_down_scaling_frc(n, q):
+    """Check if q can be upscaled to n beads and downscaled again.
+    """
     rescale = nmtransform.nm_rescale(n, q.shape[0])
     msk = get_identicals(q)
     print "Initial position of the bead system:"
@@ -50,14 +73,16 @@ numbers_to_check = range(10, 56, 9)
 def test_1_to_n():
     for n in numbers_to_check:
         q = np.array([[0.0,0.0,0.0, 1.0,0.0,0.0]])
-        yield check_up_and_down_scaling, n, q
+        yield check_up_and_down_scaling_pos, n, q
+        yield check_up_and_down_scaling_frc, n, q
 
 
 def test_2_to_n():
     for n in numbers_to_check:
         q = np.array([[0.0,0.0,0.0, 1.0,0.0,0.0],
                       [0.0,0.1,0.0, 1.0,0.1,0.0]])
-        yield check_up_and_down_scaling, n, q
+        yield check_up_and_down_scaling_pos, n, q
+        yield check_up_and_down_scaling_frc, n, q
 
 
 def test_3_to_n():
@@ -65,7 +90,8 @@ def test_3_to_n():
         q = np.array([[0.0, 0.0,0.0, 1.0, 0.0,0.0],
                       [0.0, 0.1,0.0, 1.0, 0.1,0.0],
                       [0.0,-0.1,0.0, 1.0,-0.1,0.0]])
-        yield check_up_and_down_scaling, n, q
+        yield check_up_and_down_scaling_pos, n, q
+        yield check_up_and_down_scaling_frc, n, q
 
 
 def test_4_to_n():
@@ -74,4 +100,5 @@ def test_4_to_n():
                       [0.0, 0.1,0.0, 1.0, 0.1,0.0],
                       [0.0, 0.2,0.0, 1.0, 0.2,0.0],
                       [0.0,-0.1,0.0, 1.0,-0.1,0.0]])
-        yield check_up_and_down_scaling, n, q
+        yield check_up_and_down_scaling_pos, n, q
+        yield check_up_and_down_scaling_frc, n, q
