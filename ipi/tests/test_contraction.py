117465ff95aca6f0e19ce0a5e87a41c44e8d6767
Thomas Spura <thomas.spura@gmail.com>
2013-08-05 13:49:13 +0200

    Check centroid position/force

--- ipi/tests/test_contraction.py
+++ ipi/tests/test_contraction.py
@@ -27,7 +27,7 @@ def check_up_and_down_scaling_pos(n, q):
     print q, q.shape, msk, rescale.shape
 
     # rescale up to the n beads
-    beads_n = np.dot(rescale, q)#*n/q.shape[0]
+    beads_n = np.dot(rescale, q)
     print "Upscaled to %d beads:"%n
     print beads_n, beads_n.shape
 
@@ -41,12 +41,14 @@ def check_up_and_down_scaling_pos(n, q):
             if m:
                 assert_equals(bead[i], q[0][i])
     assert_equals(q, beads_final)
+    return beads_n
 
 
 def check_up_and_down_scaling_frc(n, q):
     """Check if q can be upscaled to n beads and downscaled again.
     """
     rescale = nmtransform.nm_rescale(n, q.shape[0])
+    factor = float(n)/float(q.shape[0])
     msk = get_identicals(q)
     print "Initial position of the bead system:"
     print q, q.shape, msk
@@ -63,8 +65,31 @@ def check_up_and_down_scaling_frc(n, q):
     for bead in beads_n:
         for i, m in enumerate(msk):
             if m:
-                assert_equals(bead[i], q[0][i])
+                assert_equals(bead[i]/factor, q[0][i])
     assert_equals(q, beads_final)
+    return beads_n
+
+
+def check_centroid_pos(n, q):
+    beads_big = check_up_and_down_scaling_pos(n, q)
+    rescale_big = nmtransform.mk_rs_matrix(n, 1)
+    rescale_q = nmtransform.mk_rs_matrix(q.shape[0], 1)
+
+    centroid_big = np.dot(rescale_big, beads_big)
+    centroid_q = np.dot(rescale_q, q)
+
+    assert_equals(centroid_q, centroid_big)
+
+
+def check_centroid_frc(n, q):
+    beads_big = check_up_and_down_scaling_frc(n, q)
+    rescale_big = nmtransform.nm_rescale(n, 1)
+    rescale_q = nmtransform.nm_rescale(q.shape[0], 1)
+
+    centroid_big = rescale_big.b1tob2(beads_big)
+    centroid_q = rescale_q.b1tob2(q)
+
+    assert_equals(centroid_q, centroid_big)
 
 
 numbers_to_check = range(10, 56, 9)
@@ -75,6 +100,8 @@ def test_1_to_n():
         q = np.array([[0.0,0.0,0.0, 1.0,0.0,0.0]])
         yield check_up_and_down_scaling_pos, n, q
         yield check_up_and_down_scaling_frc, n, q
+        yield check_centroid_pos, n, q
+        yield check_centroid_frc, n, q
 
 
 def test_2_to_n():
@@ -83,6 +110,8 @@ def test_2_to_n():
                       [0.0,0.1,0.0, 1.0,0.1,0.0]])
         yield check_up_and_down_scaling_pos, n, q
         yield check_up_and_down_scaling_frc, n, q
+        yield check_centroid_pos, n, q
+        yield check_centroid_frc, n, q
 
 
 def test_3_to_n():
@@ -92,6 +121,8 @@ def test_3_to_n():
                       [0.0,-0.1,0.0, 1.0,-0.1,0.0]])
         yield check_up_and_down_scaling_pos, n, q
         yield check_up_and_down_scaling_frc, n, q
+        yield check_centroid_pos, n, q
+        yield check_centroid_frc, n, q
 
 
 def test_4_to_n():
@@ -102,3 +133,5 @@ def test_4_to_n():
                       [0.0,-0.1,0.0, 1.0,-0.1,0.0]])
         yield check_up_and_down_scaling_pos, n, q
         yield check_up_and_down_scaling_frc, n, q
+        yield check_centroid_pos, n, q
+        yield check_centroid_frc, n, q
