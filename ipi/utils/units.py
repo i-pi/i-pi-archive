483871dcb859cbf7fa8c018cd09ed7352c8be46b
Michele Ceriotti <michele.ceriotti@gmail.com>
2013-08-14 18:44:50 +0100

    Changes to the examples

--- ipi/utils/units.py
+++ ipi/utils/units.py
@@ -9,7 +9,7 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
@@ -38,7 +38,7 @@ class Constants:
       amu: Atomic mass unit.
    """
 
-   kb = 3.1668152e-06
+   kb = 1.0
    hbar = 1.0
    amu = 1822.8885
 
