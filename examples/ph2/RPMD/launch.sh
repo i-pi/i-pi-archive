915132ae309ab9b71d1c70450cc6f090e41c68ad
joshmore <joshua.more@chem.ox.ac.uk>
2013-08-22 17:32:11 +0100

    Done some tidying up of the examples directory.

--- examples/ph2/RPMD/launch.sh
+++ examples/ph2/RPMD/launch.sh
@@ -18,17 +18,17 @@
 nruns=99
 port=$1
 
-python ../../../../src/main.py input.xml &> log1
+python ../../../../i-pi input.xml &> log1
 wait
 for i in `seq 1 $nruns`; do
    newport=$((5+$port))
    j=$(($i+1))
-   sed "s/<simulation>/<simulation> <initialize> <resample_v> 25 <%resample_v> <%initialize>/" RESTART | tr "%" "/" > dummy
+   sed "s/<simulation>/<simulation> <initialize nbeads='24'> <velocities mode='thermal' units='kelvin'> 25 <%velocities> <%initialize>/" RESTART | tr "%" "/" > dummy
    sed "s/<step>8000</<step>0</; s/<port>$port/<port>$newport/; s/filename='test$i/filename='test$j/" dummy > input$i
 
    port=$(($newport)) 
 
    rm dummy
-   python ../../../../src/main.py input$i &> log$j
+   python ../../../../i-pi input$i &> log$j
    wait
 done
