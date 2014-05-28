205f7cb40e93ac42779ed6e361f0995b48172009
BingqingCheng <tonicbq@gmail.com>
2014-05-28 11:45:05 +0800

    a small program to obtain the accumulated average of the exponential estimator

---
+++ examples/lammps/h2o-pimd/process_out.py
@@ -0,0 +1,38 @@
+"""
+Calculate the accumulated average of the exponential estimator <exp(-beta*spr)>
+for the isotope fractionation. 
+
+accumulated.out:
+first column : simulation time
+second column : the instantanous exponential estimator
+third column : the accumulated average of the exponential estimator
+
+"""
+
+import numpy as np
+
+# the number of steps that allows the system to equilibrate
+equilibriumstep = 25000  
+ 
+table = np.loadtxt('no_rpc.out')
+ 
+(asize, bsize) = np.shape(table)
+print asize, bsize
+
+out = np.zeros((asize-equilibriumstep, 3))
+
+count = 0
+
+for a in range(asize):
+	
+	if a >= equilibriumstep:
+		out[count][0] = table[a][0]
+		out[count][1] = table[a][9]
+		if count == 0:
+			out[count][2] = table[a][9]
+		else:
+			out[count][2] = out[count-1][2]*count/(count+1) +table[a][9]/(count+1)					
+		count += 1
+
+np.savetxt('accumulated.out', out)
+
