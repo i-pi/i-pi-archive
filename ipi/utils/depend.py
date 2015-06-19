8492a70764fbedf0645949ca8cbeb41bd8c50e68
Ondrej Marsalek <ondrej.marsalek@gmail.com>
2015-06-19 10:26:30 -0700

    Minimize boilerplate

--- ipi/utils/depend.py
+++ ipi/utils/depend.py
@@ -19,22 +19,9 @@ must keep in step.
 For a more detailed discussion, see the reference manual.
 """
 
-# i-PI Copyright (C) 2013-2015 i-PI developers
-#
 # This file is part of i-PI.
-#
-# i-PI is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# i-PI is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http.//www.gnu.org/licenses/>.
+# i-PI Copyright (C) 2014-2015 i-PI developers
+# See the "licenses" directory for full license information.
 
 
 __all__ = ['depend_base', 'depend_value', 'depend_array', 'synchronizer',
