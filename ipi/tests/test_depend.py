c837470acb255ba3184e1b70f7fd71315732dc24
Ondrej Marsalek <ondrej.marsalek@gmail.com>
2015-09-08 16:46:22 -0700

    Tweak tests a little, update style
    Thou shall indent four spaces

--- ipi/tests/test_depend.py
+++ ipi/tests/test_depend.py
@@ -10,42 +10,49 @@ import numpy as np
 import ipi.utils.depend as dp
 
 
-a = dp.depend_array(name="a",value=np.zeros((2,2),float))
-b = dp.depend_array(name="b",value=np.zeros((2,2),float))
+a = dp.depend_array(name="a", value=np.zeros((2,2), float))
+b = dp.depend_array(name="b", value=np.zeros((2,2), float))
 
 
 def test_slicing():
-   """Depend: Slicing test"""
-   c = a[0]
-   print type(c)
-   assert(type(c) == dp.depend_array)
+    """Depend: Slicing test"""
+    c = a[0]
+    print type(c)
+    assert type(c) == dp.depend_array
+
 
 def test_addition():
-   """Depend: Addition test"""
-   c = a + b
-   print type(c)
-   assert(type(c) == np.ndarray)
+    """Depend: Addition test"""
+    c = a + b
+    print type(c)
+    assert type(c) == np.ndarray
+
 
 def test_increment():
-   """Depend: Increment test"""
-   c = np.zeros((2,2))
-   c += a
-   print type(c)
-   assert(type(c) == np.ndarray)
+    """Depend: Increment test"""
+    c = np.zeros((2,2))
+    c += a
+    print type(c)
+    assert type(c) == np.ndarray
+
 
 def test_dot():
-   """Depend: Dot test"""
-   c = np.dot(a,b)
-   print type(c)
-   assert(type(c) == dp.depend_array)
+    """Depend: Dot test"""
+    c = np.dot(a, b)
+    print type(c)
+    assert type(c) == dp.depend_array
+
 
 def test_dotf():
-   """Depend: Dot-f test"""
-   rdot = np.dot
-   def fdot(a,b):
-      return rdot(a,b).view(np.ndarray)
-   np.dot=fdot
+    """Depend: Dot-f test"""
 
-   c = np.dot(a,b)
-   assert(type(c) == np.ndarray)
-   np.dot = rdot
+    rdot = np.dot
+
+    def fdot(a, b):
+        return rdot(a, b).view(np.ndarray)
+
+    np.dot = fdot
+
+    c = np.dot(a, b)
+    assert type(c) == np.ndarray
+    np.dot = rdot
--- ipi/tests/test_interface.py
+++ ipi/tests/test_interface.py
@@ -9,15 +9,15 @@ from ipi.interfaces.sockets import Client, Driver, InterfaceSocket
 
 
 def test_client():
-   """Client: startup without socket."""
-   c = Client(_socket=False)
+    """Client: startup without socket."""
+    c = Client(_socket=False)
 
 
 def test_driver():
-   """Driver: startup without socket."""
-   d = Driver(socket=None)
+    """Driver: startup without socket."""
+    d = Driver(socket=None)
 
 
 def test_interface():
-   """InterfaceSocket: startup."""
-   i = InterfaceSocket()
+    """InterfaceSocket: startup."""
+    i = InterfaceSocket()
