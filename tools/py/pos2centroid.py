ee54d8a655bce01445c8f862809855ec82943150
Michele Ceriotti <michele.ceriotti@gmail.com>
2015-10-25 14:26:39 +0100

    Guess what? Another bug due to the IO rehaul...

--- tools/py/pos2centroid.py
+++ tools/py/pos2centroid.py
@@ -22,39 +22,37 @@ from ipi.utils.depend import *
 from ipi.utils.units import *
 
 
-def main(prefix):
+def main(prefix, suffix="pos"):
 
-   ipos=[]
-   imode=[]
-   for filename in sorted(glob.glob(prefix+".pos*")):
-      imode.append(filename.split(".")[-1])
-      ipos.append(open(filename,"r"))
+    ipos=[]
+    imode=[]
+    for filename in sorted(glob.glob(prefix+"."+suffix+"*")):
+        imode.append(filename.split(".")[-1])
+        ipos.append(open(filename,"r"))
 
-   nbeads = len(ipos)
-   natoms = 0
-   ifr = 0
-   while True:
-      try:
+    nbeads = len(ipos)
+    natoms = 0
+    ifr = 0
+    while True:
+        for i in range(nbeads):
+            try:
 
-         for i in range(nbeads):
-            if (imode[i]=="xyz"):
-               pos=read_file(imode[i],ipos[i])
-               cell = Cell()
-            else:
-               pos, cell = read_file(imode[i],ipos[i])
-            if natoms == 0:
-               natoms = pos.natoms
-               atoms = Atoms(natoms)
+                poscell = read_file(imode[i],ipos[i])
+                cell = poscell["cell"]
+                pos = poscell["atoms"]
+                if natoms == 0:
+                    natoms = pos.natoms
+                    atoms = Atoms(natoms)
 
-            atoms.q += pos.q
-            atoms.names = pos.names
-      except EOFError: # finished reading files
-         sys.exit(0)
-      atoms.q /= nbeads
-      print_file(imode[0],atoms, cell)
-      atoms.q[:]=0.0
-      ifr+=1
+                atoms.q += pos.q
+                atoms.names = pos.names
+            except EOFError: # finished reading files
+                sys.exit(0)
+        atoms.q /= nbeads
+        print_file(imode[0],atoms, cell)
+        atoms.q[:]=0.0
+        ifr+=1
 
 
 if __name__ == '__main__':
-   main(*sys.argv[1:])
+    main(*sys.argv[1:])
