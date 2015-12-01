c45bca97de81fb0a451a7684603ef35f82cc4e60
venkatkapil24 <venkat.kapil@gmail.com>
2015-12-01 14:35:12 +0100

    cleaned up the prev version.

--- ipi/engine/phonons.py
+++ ipi/engine/phonons.py
@@ -39,36 +39,26 @@ class Dynmatrix(Mover):
       oldk   : value of k in previous step.
    """
 
-   def __init__(self, fixcom=False, fixatoms=None, epsilon=0.001,oldk=0,noldbead=0,oldhessian=np.zeros(0, float)) :   
+    def __init__(self, fixcom=False, fixatoms=None, epsilon=0.001,oldk=0,noldbead=0,oldhessian=np.zeros(0, float)) :   
                  
-      """Initialises Dynmatrix.
-      Args:
-         fixcom: An optional boolean which decides whether the centre of mass
-            motion will be constrained or not. Defaults to False. 
-            THe Hessian is an array of matrix for each beads.        
-      """
+        """Initialises Dynmatrix.
+        Args:
+        fixcom: An optional boolean which decides whether the centre of mass
+                motion will be constrained or not. Defaults to False. 
+                THe Hessian is an array of matrix for each beads.        
+        """
 
-      super(Dynmatrix,self).__init__(fixcom=fixcom, fixatoms=fixatoms)
+        super(Dynmatrix,self).__init__(fixcom=fixcom, fixatoms=fixatoms)
       
-      #Finite difference option.
-      self.epsilon = epsilon
-      self.oldk = oldk
-      self.hessian = oldhessian
-      self.noldbead =noldbead
+        #Finite difference option.
+        self.epsilon = epsilon
+        self.oldk = oldk
+        self.hessian = oldhessian
+        self.noldbead =noldbead
    
 	def bind(self, ens, beads, nm, cell, bforce, bbias, prng):
       
 		super(Dynmatrix,self).bind(ens, beads, nm, cell, bforce, bbias, prng)
-      #if self.cg_old_f.shape != beads.q.size :
-      #   if self.cg_old_f.size == 0: 
-      #      self.cg_old_f = np.zeros(beads.q.size, float)
-      #   else: 
-      #      raise ValueError("Conjugate gradient force size does not match system size")
-      #if self.cg_old_d.size != beads.q.size :
-      #   if self.cg_old_d.size == 0: 
-      #      self.cg_old_d = np.zeros(beads.q.size, float)
-      #   else: 
-      #      raise ValueError("Conjugate gradient direction size does not match system size")
             
 	def step(self, k, step=None):
       """Calculates the kth derivative of force by finite differences.            
@@ -79,44 +69,27 @@ class Dynmatrix(Mover):
 
 		info("\nDynmtarix STEP %d" % step, verbosity.debug)
 	
-	#initialise des donnes du system compris par IPI
+	    #initialise des donnes du system compris par IPI
 		if(self.dforces is None) :#formations of duplicates
 			self.dbeads = self.beads.copy()
 			self.dcell = self.cell.copy()
 			self.dforces = self.bforce.copy(self.dbeads, self.dcell) 
 			
-	#initialze the vector if doesn't exit or reinitialyze to zero all components a 3N vector
+	    #initialze the vector if doesn't exit or reinitialyze to zero all components a 3N vector
 		self.delta = np.zeros(self.beads.nbeads * 3 * self.dbeads.natoms, float)	   
-		self.delta[k]=self.epsilon
-	#delta = an array with all ements equal to 0 except that kth element is epsilon.
-    #displaces kth d.o.f by epsilon.	  	  			  
+	    #delta = an array with all ements equal to 0 except that kth element is epsilon.
+        #displaces kth d.o.f by epsilon.	  	  			  
 		self.dbeads.q = self.beads.q + self.delta  #making it one raw 3N long
 		fplus = - destrip(self.dforces.f)
-		m_plus = destrip(self.beads.sm3[-1][k])
-	# displaces kth d.o.f by -epsilon.	  
+	    #displaces kth d.o.f by -epsilon.	  
 		self.dbeads.q = self.beads.q - self.delta 
-	#force of the displaced force divided by the 
 		fminus =  - destrip(self.dforces.f)
-		m_minus = destrip(self.beads.sm3[-1][k])
-	# computes the derivative with each component divided by the mass matrix
-		forces_raw = (fplus-fminus)/(2*self.epsilon*m_plus*m_minus)
-	#change the line value or add the line if does not exit to the matrix
+	    #computes a row of force-constant matrix
+		forces_raw = (fplus-fminus)/(2*self.epsilon*destrip(self.beads.sm3[-1][k]))/destrip(self.beads.sm3[-1])
+	    #change the line value or add the line if does not exit to the matrix
 		if self.hessian.shape[0] - 1 < k: #because k going from 0 to (3N-1)
 			self.hessian = np.vstack([self.hessian, forces_raw])
         else:
-	#update the hessian on the kth line if existing
+	    #update the hessian on the kth line if existing
 			self.hessian[k,:] = forces_raw
-		
-	
-		
-	
-
-	
-	   
-		 
-	   
-			  
-
-
-
 
