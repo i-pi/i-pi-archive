a4860bee7762b9af2fa941197ba5b8b42c4ac1b9
venkatkapil24 <venkat.kapil@gmail.com>
2015-12-01 15:13:23 +0100

    Input class prepared.

---
+++ ipi/inputs/phonons.py
@@ -0,0 +1,74 @@
+"""Deals with creating the ensembles class.
+
+Copyright (C) 2013, Joshua More and Michele Ceriotti
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http.//www.gnu.org/licenses/>.
+
+Inputs created by Michele Ceriotti and Benjamin Helfrecht, 2015
+
+Classes:
+   InputGeop: Deals with creating the Geop object from a file, and
+      writing the checkpoints.
+"""
+
+import numpy as np
+import ipi.engine.initializer
+from ipi.engine.mover import *
+from ipi.utils.inputvalue import *
+from ipi.inputs.thermostats import *
+from ipi.inputs.initializer import *
+from ipi.utils.units import *
+
+__all__ = ['InputGeop']
+
+class InputPhonons(InputDictionary):
+    """Dynamic matrix calculation options.
+    
+       Contains options related with finite difference computation of force constats. 
+
+    """
+
+    #TODO: RENAME BFGS/L-BFGS ONLY OPTIONS TO INDICATE SPECIFIC TO BFGS/L-BFGS
+    attribs={"epsilon"  : (InputAttribute, {"dtype"   : float, "default": 0.01, 
+                                    "help"    : "The finite deviation used to compute deribvative of force."
+                                    }) }
+   
+#    fields = { 
+#                "tolerances" : ( InputDictionary, {"dtype" : float, 
+#                              "options" : [ "energy", "force", "position" ],
+#                              "default" : [ 1e-8, 1e-8, 1e-8 ],
+#                              "dimension": [ "energy", "force", "length" ] }),
+#                "cg_old_force": (InputArray, {"dtype" : float,
+#                              "default"   : input_default(factory=np.zeros, args = (0,)),
+#                              "help"      : "The previous force in a CG optimization.",
+#                              "dimension" : "force"}),
+#                              "help"    : "The maximum step size for BFGS line minimizations."}),
+#                "invhessian" : (InputArray, {"dtype" : float, 
+#                              "default" : input_default(factory=np.eye, args = (0,)),
+#                              "help"    : "Approximate inverse Hessian for BFGS, if known."})
+#                     }
+                   
+    dynamic = {  }
+
+    default_help = "Fill in."
+    default_label = "PHONONS"
+
+    def store(self, geop):
+        if geop == {}: return
+        self.epsilon.store(phonons.epsilon)
+        
+    def fetch(self):		
+        rv = super(phonons,self).fetch()
+        rv.epsilon = self.epsilon.fetch()        
+        return rv
