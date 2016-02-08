db7ab36de2b670276f9436f9d23562e3dfdb041e
Michele Ceriotti <michele.ceriotti@gmail.com>
2016-02-08 14:26:14 +0100

    Holy shit. This was stupid.

--- ipi/engine/thermostats.py
+++ ipi/engine/thermostats.py
@@ -577,7 +577,7 @@ class ThermoGLE(Thermostat):
             depend_value(name='C', func=self.get_C,
                dependencies=[dget(self,"temp")]))
       else:
-         dset(self,"C",depend_value(value=0.5*C.copy(),name='C'))
+         dset(self,"C",depend_value(value=C.copy(),name='C'))
 
       dset(self,"T",
          depend_value(name="T",func=self.get_T,
