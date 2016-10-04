cf75e6f014689f30e130a9d0430c3be38424ef37
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2016-10-04 15:20:16 +0200

    read_xyz tested and fixed

--- ipi_tests/utils/io/backends/test_io_xyz.py
+++ ipi_tests/utils/io/backends/test_io_xyz.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2
+# pylint: disable=C0111,W0621,R0914,C0301
+#+easier to find important problems
 
+import re
 import pytest
 import numpy as np
 import numpy.testing as npt
-import re
 
 import ipi_tests.utils.io.backends.io_xyz_utils as xyz_gen
 import ipi.utils.io.backends.io_xyz as io_xyz
@@ -16,9 +18,9 @@ import ipi.utils.mathtools as mt
 
 deg2rad = np.pi/180.0
 cell_string = ' '.join([str(x) for x in mt.abc2h(5.1, 5.2, 5.0,
-                                                      91*deg2rad,
-                                                      89*deg2rad,
-                                                      90*deg2rad).flatten()])
+                                                 91*deg2rad,
+                                                 89*deg2rad,
+                                                 90*deg2rad).flatten()])
 default_cell_mat = mt.abc2h(1.0, 1.0, 1.0, np.pi/2.0, np.pi/2.0, np.pi/2.0)
 
 # natoms, frames, comment, expected_cell, precision
@@ -49,15 +51,16 @@ def create_random_xyz_traj(request):
                                                           frames, comment)
     filedesc.seek(0)
 
-    check_comment = re.match('# CELL[\(\[\{]H[\)\]\}]: ([-0-9\.?Ee ]*)\s*', comment)
+    check_comment = re.match('# CELL[\(\[\{]H[\)\]\}]: ([-0-9\.?Ee ]*)\s*', comment) #pylint: disable=W1401
+
     if check_comment:
-        genh = np.array(check_comment.group(1).split()[:9], float).reshape((3,3))
+        genh = np.array(check_comment.group(1).split()[:9], float).reshape((3, 3))
         invgenh = np.linalg.inv(genh)
-        for ui in xrange(natoms*frames):
-            u = np.array([xyz[3*ui], xyz[3*ui+1], xyz[3*ui+2]])
-            us = np.dot(u,invgenh)
-            u = np.dot(expected_cell, us)
-            xyz[3*ui], xyz[3*ui+1], xyz[3*ui+2] = u
+        for _ui in xrange(natoms*frames):
+            _uu = np.array([xyz[3*_ui], xyz[3*_ui+1], xyz[3*_ui+2]])
+            _us = np.dot(_uu, invgenh)
+            _uu = np.dot(expected_cell, _us)
+            xyz[3*_ui], xyz[3*_ui+1], xyz[3*_ui+2] = _uu
 
 
     return (filedesc, xyz, atom_names, natoms, frames,
@@ -73,8 +76,6 @@ def test_read_xyz(create_random_xyz_traj):
 
         tcomment, tcell, tqatoms, tnames, tmasses = io_xyz.read_xyz(filedesc)
 
-        check_comment = re.match('# CELL[\(\[\{]H[\)\]\}]: ([-0-9\.?Ee ]*)\s*', comment)
-
         assert tcomment.strip() == comment
         npt.assert_array_almost_equal(tqatoms, xyz[_fr*natoms*3:_fr*3*natoms+3*natoms], decimal=precision)
         npt.assert_array_equal(np.array(atom_names[_fr*natoms:_fr*natoms+natoms], dtype='|S4'), tnames)
@@ -82,14 +83,15 @@ def test_read_xyz(create_random_xyz_traj):
 
 def test_iter_xyz(create_random_xyz_traj):
     filedesc, xyz, atom_names, \
-        natoms, junk, comment, expected_comment, precision = create_random_xyz_traj
+        natoms, junk, comment, expected_cell, precision = create_random_xyz_traj
 
     _fr = 0
-    for io in io_xyz.iter_xyz(filedesc):
-        tcomment, tcell, tqatoms, tnames, tmasses = io
+    for _io in io_xyz.iter_xyz(filedesc):
+        tcomment, tcell, tqatoms, tnames, tmasses = _io
         assert tcomment.strip() == comment
         npt.assert_array_almost_equal(tqatoms, xyz[_fr*natoms*3:_fr*3*natoms+3*natoms], decimal=precision)
-        npt.assert_array_equal( np.array(atom_names[_fr*natoms:_fr*natoms+natoms], dtype='|S4'), tnames)
+        npt.assert_array_equal(np.array(atom_names[_fr*natoms:_fr*natoms+natoms], dtype='|S4'), tnames)
+        npt.assert_array_almost_equal(tcell, expected_cell, decimal=precision)
         _fr += 1
 
 
