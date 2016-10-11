ee1f25a9406af7bd7c472d24ba29fbeb72d96213
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2016-10-11 12:52:55 +0200

    properties_Trajectory_print_file tested

--- ipi_tests/engine/test_properties.py
+++ ipi_tests/engine/test_properties.py
@@ -1,87 +1,114 @@
 #!/usr/bin/env python2
 
-import pytest
 import mock
 import tempfile
+import re
+
+import pytest
+from pytest_mock import mocker
+
+import numpy as np
+import numpy.testing as npt
 
 import ipi.engine.properties
-import numpy as np
+from ipi_tests import xyz_generator as xyz_gen
 
 
+test_Trajectories_print_traj_prms = [
+    #natoms, nbeads, bead, cell, cell_units, property_, format_, units_conversion
+    (1, 1, 0, np.random.rand(9), 'atomic_unit', 'positions', 'xyz', 1),
+    (1, 1, 0, np.random.rand(9), 'angstrom', 'positions', 'xyz', 0.52917721),
+    (1, 1, 0, np.random.rand(9), 'atomic_unit', 'forces', 'xyz', 1),
+    (1, 1, 0, np.random.rand(9), 'angstrom', 'forces', 'xyz', 0.52917721),
+    (1, 1, 0, np.random.rand(9), 'atomic_unit', 'positions', 'pdb', 1),
+    (1, 1, 0, np.random.rand(9), 'angstrom', 'positions', 'pdb', 0.52917721),
+    (1, 1, 0, np.random.rand(9), 'atomic_unit', 'forces', 'pdb', 1),
+    (1, 1, 0, np.random.rand(9), 'angstrom', 'forces', 'pdb', 0.52917721),
 
-def create_a_fake_system_obj():
+    (5, 1, 0, np.random.rand(9), 'atomic_unit', 'positions', 'xyz', 1),
+    (5, 1, 0, np.random.rand(9), 'angstrom', 'positions', 'xyz', 0.52917721),
+    (5, 1, 0, np.random.rand(9), 'atomic_unit', 'forces', 'xyz', 1),
+    (5, 1, 0, np.random.rand(9), 'angstrom', 'forces', 'xyz', 0.52917721),
+    (5, 1, 0, np.random.rand(9), 'atomic_unit', 'positions', 'pdb', 1),
+    (5, 1, 0, np.random.rand(9), 'angstrom', 'positions', 'pdb', 0.52917721),
+    (5, 1, 0, np.random.rand(9), 'atomic_unit', 'forces', 'pdb', 1),
+    (5, 1, 0, np.random.rand(9), 'angstrom', 'forces', 'pdb', 0.52917721),
 
-    natoms, q, atom_names, cell = 1, np.random.rand(3), ['Na'], np.random.rand(3,3)
+    (5, 10, 1, np.random.rand(9), 'atomic_unit', 'positions', 'xyz', 1),
+    (5, 10, 2, np.random.rand(9), 'angstrom', 'positions', 'xyz', 0.52917721),
+    (5, 10, 3, np.random.rand(9), 'atomic_unit', 'forces', 'xyz', 1),
+    (5, 10, 4, np.random.rand(9), 'angstrom', 'forces', 'xyz', 0.52917721),
+    (5, 10, 5, np.random.rand(9), 'atomic_unit', 'positions', 'pdb', 1),
+    (5, 10, 6, np.random.rand(9), 'angstrom', 'positions', 'pdb', 0.52917721),
+    (5, 10, 7, np.random.rand(9), 'atomic_unit', 'forces', 'pdb', 1),
+    (5, 10, 8, np.random.rand(9), 'angstrom', 'forces', 'pdb', 0.52917721),
+]
+
+
+def create_a_fake_system_obj(natoms, q, forces, atom_names, cell):
 
     sys_attr = {
         'beads.natoms': natoms,
         'beads.q': q,
         'simul.step': 0,
-        'beads.names': atom_names,
-        'cell.h': cell
+        'beads.names': atom_names[:natoms],
+        'cell.h': cell,
+        'forces.f': forces,
         }
 
     sys = mock.Mock(**sys_attr)
-    # sys.beads.natoms = 'asdasd'
+
+    return sys
+
+@pytest.fixture(params=test_Trajectories_print_traj_prms)
+def prepare_Trajectories_print_traj(request):
+
+    natoms, nbeads, bead, cell, cell_units, property_, format_, unit_conv = request.param
+
+    junk, xyz, atom_names = xyz_gen.xyz_traj(natoms, nbeads, 'comment')
+
+    # Using the same xyz_generator to generate random forces
+    junk, forces, atom_names = xyz_gen.xyz_traj(natoms, nbeads, 'comment')
+    xyz = xyz.reshape((nbeads, natoms*3))
+    forces = forces.reshape((nbeads, natoms*3))
+    cell = cell.reshape((3,3))
+
+    system_mock = create_a_fake_system_obj(natoms, xyz, forces, atom_names[:natoms], cell)
 
     stream = tempfile.NamedTemporaryFile(mode='w')
 
-    return sys, stream
+    if property_ == 'forces':
+        expected_position = forces.copy()
+    elif property_ == 'positions':
+        expected_position = xyz.copy()
+    expected_cell = cell.copy()
 
-@mock.patch('ipi.engine.properties.io.print_file', autospec=True)
-def test_Trajectories_print_traj(mock_io):
+    expected_comment = re.compile(r'^\s*\{%s\}\s+Traj\:\s*%s\s+Step\:\s+%i\s*Bead\:\s*%i\s*$' % (cell_units, property_, 1, bead))
 
-    system, stream = create_a_fake_system_obj()
+    expected_names = atom_names[:]
+
+    return system_mock, stream, bead, expected_position, expected_cell, expected_comment, expected_names, format_, property_, cell_units, unit_conv
+
+def test_Trajectories_print_traj(prepare_Trajectories_print_traj, mocker):
+
+    mock_io = mocker.patch('ipi.engine.properties.io.print_file', autospec=True)
+
+
+    system, stream, bead, expected_position, expected_cell, expected_comment, expected_names, format_, property_, cell_units, unit_conv = prepare_Trajectories_print_traj
+
     ipi.engine.properties.io.print_file = mock_io
 
     trj = ipi.engine.properties.Trajectories()
     trj.bind(system)
 
+    ########### Function to test call
+    trj.print_traj(property_, stream, b=bead, format=format_, cell_units=cell_units, flush=True )
 
-    trj.print_traj('positions', stream, b=0, format='xyz', cell_units='atomic_unit', flush=True )
+    comment = mock_io.call_args[1]['title']
+    file_type, atoms, cell, junk = mock_io.call_args[0]
 
-    print mock_io.call_args_list
-    #Just check that the arguments are the right ones!
-
-
-
-
-test_Trajectories_print_traj()
-
-
-# def test_create_a_fake_system_obj(monkeypatch):
-#     natoms = 3
-#     nbeads = 2
-
-#     # per frames
-#     q = ['random_number_'+str(x) for x in xrange(nbeads)]
-#     qc = ['randoms_numbers']
-#     f = ['random_number_'+str(x) for x in xrange(nbeads)]
-#     temperature = 'random_number'
-
-#     # need_to_sett = ['beads', 'cell']
-
-#     # for _att in need_to_sett:
-#     #     monkeypatch.setattr('ipi.engine.system', need_to_sett, raising=False)
-
-
-#     set_values = {
-#         'beads.natoms' : natoms,
-#         'beads.nbeads' : nbeads,
-#         'beads.q' : q,
-#         'forces.f' : f,
-#         'ensemble.temp': temperature,
-#     }
-
-#     for _key, _val in set_values.items():
-#         attr_list = _key.split(('.'))
-#         for _ii, _sk in enumerate(attr_list):
-#             attr = '.'.join(attr_list[:_ii])
-
-#             if not hasattr(ipi.engine.system, attr):
-#                 if _ii == len(attr_list):
-#                     monkeypatch.setattr('ipi.engine.system', attr, value=_val, raising=False)
-#                 else:
-#                     monkeypatch.setattr('ipi.engine.system', attr, raising=False)
-
-#     print ipi.engine.system.beads.q
+    assert file_type == format_
+    assert expected_comment.match(comment)
+    npt.assert_almost_equal(atoms.q, expected_position[bead], 5)
+    npt.assert_equal(atoms.names, expected_names[:system.beads.natoms])
+    npt.assert_almost_equal(cell.h, expected_cell * unit_conv)
