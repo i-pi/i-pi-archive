ae49c5e39ee6bbea0f06524a49674cb9fc3040c2
venkatkapil24 <venkat.kapil@gmail.com>
2016-11-10 11:37:24 +0100

    merged and modified examples

--- ipi/engine/forces.py
+++ ipi/engine/forces.py
@@ -796,12 +796,10 @@ class Forces(dobject):
              for k in range(self.nbeads/2): # only compute the elements that will not be set to zero when multiplying by alpha
                  fsc[2*k+1] = 2*(fplusminus[self.nbeads/2+k]-fplusminus[k])/(2.0*delta)
          else:
-             print "for alpha =0 forward difference with", delta, self.mforces[-1].epsilon
              for k in range(self.nbeads/2): # do forward differences only
                  fsc[2*k+1] = 2*(fplusminus[self.nbeads/2+k]-fbase[2*k+1])/delta
       else: 
          # standard, more expensive version (alpha=1 could also be accelerated but is not used in practice so laziness prevails)
-         print "for alpha !=0 forward difference with", delta, self.mforces[-1].epsilon
          self.dbeads.q = self.beads.q + delta*fbase/self.beads.m3 # move forward
          fplus = depstrip(self.dforces.f).copy()
          self.dbeads.q = self.beads.q - delta*fbase/self.beads.m3 # move backwards
