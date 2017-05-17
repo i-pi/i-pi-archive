817725b651d22dcec1cafc7649dc7ec0a5e28c7c
venkatkapil24 <venkat.kapil@gmail.com>
2017-05-17 11:30:34 +0200

    cleaned up.

--- tools/py/getvvac.py
+++ tools/py/getvvac.py
@@ -1,4 +1,4 @@
-#!*/usr/bin/env python2
+#!/usr/bin/env python2
 
 """ getvvac.py
 
@@ -20,36 +20,31 @@ from ipi.utils.depend import *
 from ipi.utils.units import *
 
 
-def main(prefix, mlag, label):
+def main(prefix, mlag, pad, label=None):
    # TODO: This really needs an argument parser to allow for several labels 
    mlag = int(mlag)
+   npad = int(pad)
 
 
    ff = open(prefix+".vc.xyz")
    rr = read_file("xyz", ff, output="array")
    threenatoms = len(rr['data'])
    natoms = threenatoms/3
-   labelbool = rr['names'] == label
-   # testhack
-   #a=rr['names'] == "O"
-   #b=rr['names'] == "H"
-   #labelbool=a+b
-   # end testhack
+   labelbool = np.ones(natoms, bool)
+   if(label != None):
+      labelbool = rr['names'] == label
    ff.close()
 
    ifile=open(prefix+".vc.xyz")
-   ofile=prefix+"_"+str(mlag)+".vvft"
-   ofile2=prefix+"_"+str(mlag)+"_window.vvft"
-   ofile3=prefix+"_"+str(mlag)+".vvac"
+   ofile=prefix + "_" + str(mlag) + ".vvft"
 
 
    vel = np.zeros((2*mlag, labelbool.sum(), 3) , float)
-   rvvac = np.zeros(2*mlag, float)
-   omega = np.asarray(range(2*mlag))/float(2*mlag)
+   rvvac = np.zeros(2*mlag + npad, float)
+   omega = np.asarray(range(2*mlag + npad) )/float(2*mlag + npad)
    time = np.asarray(range(mlag))
+   window=np.bartlett(2*mlag).reshape((2*mlag,1,1))
    count = 0
-# TODO: pad should become an argument as well
-   pad=10000
 
    while True:
 
@@ -60,34 +55,17 @@ def main(prefix, mlag, label):
 
         vel = vel - np.mean(vel, axis=0)
 
-        tmp = np.fft.fft(vel, axis=0, norm="ortho")
+        tmp = np.fft.fft(vel*window, n=2*mlag+npad , axis=0, norm="ortho")
         tmp = tmp * np.conjugate(tmp)
         tmp = np.real(np.mean(tmp, axis=(1,2)))
         rvvac = rvvac + tmp
         count = count + 1
+        print count
 
      except EOFError:
         break
-
-# Go for windowing and padding
-# First the autocorrelation function. "ortho" gives back the 1/sqrt(mlag) factor
-   autocorr=np.fft.ihfft(rvvac, norm="ortho")[:mlag]
-# Let's try first the triangular window
-   window=np.bartlett(2.*mlag)[mlag:]
-# Now multiply them in real time (could we do it in Fourier space?)
-   autowindow=np.array([i*j for i,j in zip(window, autocorr)])
-# And finally, re-fourier transform assuming the signal is hermitian in order to avoid noise
-# Giving n greater than the array dimension pads it with zeroes
-   windowvvac=np.fft.hfft(autowindow, n=2*mlag+pad, norm="ortho")
-
-   omegapad = np.asarray(range(2*mlag+pad))/float(2*mlag+pad)
-
-   np.savetxt(ofile3, np.vstack((np.real(time), np.real(autocorr), np.real(autowindow[:mlag]))).T[0:2*mlag])
- 
-   np.savetxt(ofile, np.vstack((np.real(omega), np.real(rvvac/count))).T[0:mlag])
-
-# normalize to maintain same area as before padding and account for the various 2*pi factors
-   np.savetxt(ofile2, np.vstack((np.real(omegapad), np.real(windowvvac*(mlag+pad)/(count*2.*np.pi*mlag)))).T[0:2*mlag])
+   
+   np.savetxt(ofile, np.vstack((np.real(omega)[0:2*mlag], np.real(rvvac/count)[0:2*mlag] * (mlag + npad) / mlag )).T[0:mlag])
 
 if __name__ == '__main__':
    main(*sys.argv[1:])
