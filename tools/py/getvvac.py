c61c279ccd1c678a276ea1283d844d985911d410
venkatkapil24 <venkat.kapil@gmail.com>
2017-05-22 10:12:22 +0200

    added padding which gives a slightly suspicious result.

--- tools/py/getvvac.py
+++ tools/py/getvvac.py
@@ -25,10 +25,10 @@ def main(prefix, mlag, pad, label=None):
    mlag = int(mlag)
    npad = int(pad)
 
-   ff = open(prefix+".vc.xyz")
-   rr = read_file("xyz", ff, output="array")
+   ff = open(prefix + ".vc.xyz")
+   rr = read_file("xyz", ff, output = "array")
    threenatoms = len(rr['data'])
-   natoms = threenatoms/3
+   natoms = threenatoms / 3
    labelbool = np.ones(natoms, bool)
    if(label != None):
       labelbool = rr['names'] == label
@@ -38,11 +38,10 @@ def main(prefix, mlag, pad, label=None):
    ofile1 = prefix + "_" + "raw" + "_" + str(mlag) + ".vvft"
    ofile2 = prefix + "_" + "win" + "_" + str(mlag) + "_" + str(npad) + ".vvft"
 
-
-   vel = np.zeros((2*mlag, labelbool.sum(), 3) , float)
-   fvvac = np.zeros(2*mlag, float)
-   omega = np.asarray(range(2*mlag))/float(2*mlag)
-   dt = 1.0/float(2*mlag)
+   vel = np.zeros((2 * mlag, labelbool.sum(), 3) , float)
+   fvvac = np.zeros((2 * mlag) / 2 + 1, float)
+   omega = np.asarray(range(len(fvvac)))/float(len(fvvac))
+   dt = 1.0 / float(2 * mlag)
    count = 0
 
    while True:
@@ -52,19 +51,23 @@ def main(prefix, mlag, pad, label=None):
             rr = read_file("xyz", ifile, output="array")
             vel[i] = rr['data'].reshape(natoms,3)[labelbool]
 
-        vel = (vel - np.mean(vel, axis = 0)) 
-        fvel = np.fft.fft(vel, axis = 0)
+        fvel = np.fft.rfft(vel , axis = 0)
         tfvvac = fvel * np.conjugate(fvel)
 
-        fvvac = fvvac + 3.0 * np.real(np.mean(tfvvac, axis = (1,2))) * dt / (2 * np.pi) 
+        fvvac = fvvac + 3.0 * np.real(np.mean(tfvvac, axis = (1,2))) * dt / (2 * np.pi) # / (2 * mlag + npad) / 2 * mlag
         count = count + 1
 
      except EOFError:
         break
 
    fvvac = np.real(fvvac) / count 
-   np.savetxt(ofile1, np.vstack((omega, fvvac)).T[:mlag])
+   np.savetxt(ofile1, np.vstack((omega, fvvac)).T)
 
+   vvac = np.fft.ihfft(fvvac) * np.bartlett(2 * mlag)[mlag:]
+   pfvvac = np.real(np.fft.hfft(vvac , axis = 0, n = 2 * mlag + npad))[:2 * mlag]
+   omega = (np.asarray(range(2 * mlag + npad))/float(2 * mlag + npad))[:2 * mlag]
+   np.savetxt(ofile2, np.vstack((omega, pfvvac)).T[:mlag])
+   
 
 if __name__ == '__main__':
    main(*sys.argv[1:])
