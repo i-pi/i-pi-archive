686f37c54d23103595ddcaa80316050adf4ed2f8
venkatkapil24 <venkat.kapil@gmail.com>
2017-08-29 18:22:47 +0200

    creates an output file for each bead when printing momenta

--- ipi/engine/outputs.py
+++ ipi/engine/outputs.py
@@ -248,7 +248,7 @@ class TrajectoryOutput(dobject):
       # including underscpre
       fmt_bead = "{0:0" + str(int(1 + np.floor(np.log(self.system.beads.nbeads)/np.log(10)))) + "d}"
 
-      if getkey(self.what) in ["positions", "velocities", "forces", "extras", "forces_sc"]:
+      if getkey(self.what) in ["positions", "velocities", "forces", "extras", "forces_sc" , "momenta"]:
 
          # must write out trajectories for each bead, so must create b streams
 
