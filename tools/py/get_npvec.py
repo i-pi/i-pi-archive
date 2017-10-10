61ec7a070ba1165b25ad03c60d29d6421ca52b91
Alice Cuzzocrea <alice.cuzzocrea@gmail.com>
2017-10-10 17:27:41 +0200

    added cut-off for the grid in the 3D histogram evaluation

--- tools/py/get_npvec.py
+++ tools/py/get_npvec.py
@@ -1,36 +1,44 @@
 #!/usr/bin/env python2 
+"""
+Computes the momentum distribution having as input the end-to-end distances in atomic units.
+The results are in atomic units.
 
-description = """
-   Computes the 3D momentum distribution having as input the end-to-end distances in atomic units,
-   stored as a file with one sample per row, e.g. 
-      Delta_x  Delta_y  Delta_z
-      ....
-   The output contains both the histogram of deltas 
-   Dx Dy Dz n(D) 
-   and the 3D PMD in atomic units, printed as a grid file
-   px py pz n(p)   
 """
 
 import argparse
 import numpy as np
+import time
 
-def histo3d(data, xg, yg, zg, sigma):
-    ly=xg*0.0
+
+def histo3d(data, delta, ns, cut, dqstep, mean, sigma):
+    ly=delta[0,:]*0.0
+    lspread = np.arange(-int(cut/sigma/dqstep), int(cut/sigma/dqstep)+1)        
+    print lspread 
+    index = np.zeros(len(lspread))
     for x in data:
-        ly += np.exp(-((xg-x[0])**2 + (yg-x[1])**2+ (zg-x[2])**2)*(0.5*sigma**2))
+        qx= int(x[0]/dqstep + ns/2.)*ns*ns
+        qy= int(x[1]/dqstep + ns/2.)*ns
+        qz= int(x[2]/dqstep + ns/2.) 
+        gx,gy,gz =np.meshgrid(qx + lspread*ns*ns, qy + lspread*ns, qz + lspread )
+        index = (np.array(gx)).flatten() + (np.array(gy)).flatten() + (np.array(gz)).flatten()
+        y = np.where((index < ns**3) & (index >= 0))
+        ly[index[y]] += np.exp(-((delta[0, index[y]]-x[0])**2 + (delta[1, index[y]]- x[1])**2 + (delta[2, index[y]] - x[2])**2)*(0.5*sigma**2))
     return ly
-
-def get_np(prefix, fname, bsize, P, m, Tkelv, nskip, s, ns):   
-
+     
+def get_np(path, fname, bsize, P, m, Tkelv, nskip, s, ns, cut):   
+    start = time.clock()
     T= 3.1668105e-06*Tkelv
-    print "Kernel width: %10.5e - Grid spacing: %10.5e" % (1/np.sqrt(T * P * m), 2*s/(ns-1))
     dq = np.zeros((bsize,3) , float)
     dqxgrid = np.linspace(-s, s, ns)
     dqygrid = np.linspace(-s, s, ns)
     dqzgrid = np.linspace(-s, s, ns)
-    #make a meshgrid for points
-    xxx, yyy, zzz = np.meshgrid(dqxgrid, dqygrid, dqzgrid)
-
+    dqstep= np.abs(dqxgrid[1]-dqxgrid[0])
+    print dqstep, 1./np.sqrt(T * P * m)
+    xxx,yyy,zzz= np.meshgrid(dqxgrid, dqygrid, dqzgrid)
+    qgrid= np.zeros((3,ns*ns*ns))
+    qgrid[0,:] = (np.array(yyy)).flatten()
+    qgrid[1,:] = (np.array(xxx)).flatten()
+    qgrid[2,:] = (np.array(zzz)).flatten()
 
     # Defines the final grid for momentum.
     pxi = -np.pi/(dqxgrid[1]-dqxgrid[0])
@@ -51,56 +59,50 @@ def get_np(prefix, fname, bsize, P, m, Tkelv, nskip, s, ns):
     pzgrid = np.linspace(pzi,pzf,ns)
     pzgrid= pzgrid- pzstep/2.
  
-    pgrid= []
-    for x in range(len(pxgrid)):
-       for y in range(len(pygrid)):
-          for z in range(len(pzgrid)):                  
-             pgrid.append([pxgrid[x],pygrid[y],pzgrid[z]])
-
-    pgrid= np.asarray(pgrid)
-
+    px,py,pz= np.meshgrid(pxgrid, pygrid, pzgrid)
+    pgrid= np.zeros((3,ns*ns*ns))
+    pgrid[0,:] = (np.array(py)).flatten()
+    pgrid[1,:] = (np.array(px)).flatten()
+    pgrid[2,:] = (np.array(pz)).flatten() 
+    dpstep= np.abs(pxgrid[1]-pxgrid[0])
 
     #Read the end to end distances from file
-    data_path =str(fname)
-    delta= np.loadtxt(data_path)[nskip:]
-    ndata = np.shape(delta)[0] 
+    data_path =str(path + fname)
+    delta= np.loadtxt(data_path)
+    step = np.shape(delta)[0] 
     
-    if bsize<0: # automatically determine block size -- if it's a negative number interpret as the number of blocks
-        bsize = int(ndata/abs(bsize))
-    nplist3d=[]    
-    n_block =int(ndata/bsize)
+    nplist3d=[]
+    n_block =int(step/bsize)
     if (n_block ==0):
-        print 'not enough data to build a block'
-        exit()
+             print 'not enough data to build a block'
+             exit()
     for x in xrange(n_block):
         dq = delta[x*bsize : (x+1)*bsize]
-        h3d = histo3d(np.concatenate((dq, -dq)), xxx, yyy, zzz, np.sqrt(T * P * m)) 
-               
+        h3d = histo3d(np.concatenate((dq, -dq)), qgrid, ns, cut, dqstep, 0, np.sqrt(T * P * m)) 
         # Computes the 3D Fourier transform of the histogram of the end-to-end distances.
-        npvec3d= np.abs(np.fft.fftshift(np.fft.fftn(h3d)))       
-
-        nplist3d.append(np.reshape(npvec3d,(ns*ns*ns)))
+        npvec3d= np.abs(np.fft.fftshift(np.fft.fftn(np.reshape(h3d, (ns, ns, ns)))))       
+        nplist3d.append(npvec3d.flatten())
         
     avgnp3d= np.mean(np.asarray(nplist3d), axis = 0)
-    norm=np.sum(avgnp3d)
+    norm= sum(avgnp3d*dpstep**3)
     avgnp3d= avgnp3d/norm
-    
-    np.savetxt(str(prefix + ".np"), np.c_[pgrid,avgnp3d])
- 
+    np.savetxt(str(path + "np3d.data"), np.c_[pgrid.T, avgnp3d])
+    print time.clock()-start 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("-T", type=float, required=True, help="Specify the temperature of the system in Kelvin [required]")
-    parser.add_argument("-P", type=int, required=True, help="Specify the number of beads [required]")
-    parser.add_argument("-m", type=float, default= 1837 , help="Specify the mass of the atom in atomic units-default is hydrogen")
-    parser.add_argument("-block", type=int, default=-4, help="Specify the size of the blocks [defaults: 1/4th of the trajectory]")
-    parser.add_argument("--prefix",type=str, default="pmd3d", help="Prefix for output files")
-    parser.add_argument("--fname",type=str,default="", help="name of the end-to-end distances file")    
-    parser.add_argument("--skip", type=int, default= 50 , help="Removes the equilibration steps")
-    parser.add_argument("-dint", type=float, default=15.0, help="Specify the positive extrema of the interval to build the histogram ([-dint,dint])")
-    parser.add_argument("-ns", type=int, default=60, help="Specify the number of point to use for the histogram")
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument("--path",type=str, default="", help="path of the folder conatining the end-to-end distances file")
+    parser.add_argument("--fname",type=str,default="", help="name of the end-to-end distances file")
+    parser.add_argument("-bsize", type=int, default=50000, help="Specify the size of the blocks")
+    parser.add_argument("-P", type=int, default= 1 , help="Specify the number of beads")
+    parser.add_argument("-m", type=float, default= 1837 , help="Specify the mass of the atom in atomic units-default is hydorgen")
+    parser.add_argument("-T", type=float, default= 300 , help="Specify the temperature of the system in kelvin")
+    parser.add_argument("-nskip", type=int, default= 500 , help="Removes the equilibration steps")
+    parser.add_argument("-dint", type=float, default=10.0, help="Specify the positive extrema of the interval to build the histogram ([-dint,dint])")
+    parser.add_argument("-ns", type=int, default=150, help="Specify the number of point to use for the histogram")
+    parser.add_argument("-cut", type=int, default=15, help="Specify the size of the grid around a specific point in units of sigma")
     args = parser.parse_args()
 
-    get_np(args.prefix, args.fname, args.block, args.P, args.m, args.T, args.skip, args.dint, args.ns)
+    get_np(args.path, args.fname, args.bsize, args.P, args.m, args.T, args.nskip, args.dint, args.ns, args.cut)
 
 
