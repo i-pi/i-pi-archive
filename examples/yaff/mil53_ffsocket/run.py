f2d6144944ed7102beea064e858090c44337a620
Przemyslaw Juda <przemyslaw.juda@gmail.com>
2017-11-15 09:19:54 +0100

    Yaff examples adapted, testing should follow

--- examples/yaff/mil53_ffsocket/run.py
+++ examples/yaff/mil53_ffsocket/run.py
@@ -10,7 +10,7 @@ if __name__=='__main__':
     logf = open('yaff_%s.log'%os.getpid(),'w')
     log._file = logf
     servername = 'MIL53'
-    system = System.from_file('MIL53.chk')
+    system = System.from_file('init.chk')
     ff = ForceField.generate(system, 'pars.txt', rcut=15*angstrom, alpha_scale=3.2, gcut_scale=1.5, smooth_ei=True)
 
     socket = MySocket(servername, verbose=True)
