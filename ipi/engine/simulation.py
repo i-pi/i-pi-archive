d3e5f29e88410289aea8e03560f2ede406b7e462
Venkat Kapil <venkat.kapil@gmail.com>
2017-12-13 18:23:36 +0530

    merged with the latest changes to the reg-test

--- ipi/engine/simulation.py
+++ ipi/engine/simulation.py
@@ -210,13 +210,7 @@ class Simulation(dobject):
 
         for k, f in self.fflist.iteritems():
             f.run()
-<<<<<<< HEAD
-
-
-=======
-            
                     
->>>>>>> remd
         # prints inital configuration -- only if we are not restarting
         if self.step == 0:
             self.step = -1
