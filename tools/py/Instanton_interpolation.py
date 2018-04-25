Remove old example. Fix small bug in hessian interpolation

tools/py/Instanton_interpolation.py
@@ -172,12 +172,14 @@ if input_hess != 'None' or chk !='None':
     size2 = size0 *  nbeadsNew
 
     new_h = np.zeros([size0, size2])
+    
     for i in range(size0):
         for j in range(size0):
             h = np.array([])
             for n in range(nbeads):
                 h = np.append(h, hessian[i, j + size0 * n])
-            diag = rpc.b1tob2(h)
+            h3=np.concatenate((h,h,h),axis=0).reshape((h.size,3),order='F') #Open path expect three coordinates per atom
+            diag = rpc.b1tob2(h3)[:,0]
             new_h[i, j:size2:size0] += diag
 
     #new_h_half = new_h[:, 0:size2 / 2]